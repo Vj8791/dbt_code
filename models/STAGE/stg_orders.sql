9453ae1db0af019c117229f1bf6c3475627b4592
Vijay S <vijaysha8791@gmail.com>
2024-05-12 12:53:50 +0000

    hh

--- models/STAGE/stg_orders.sql
+++ models/STAGE/stg_orders.sql
@@ -1,2 +1,10 @@
-select *, (ORDER_SELLING_PRICE -ORDER_COST_PRICE ) Order_profit 
-from {{ ref('raw_order') }}
+select o.*
+    , p.*
+    , (o.ORDER_COST_PRICE - o.order_selling_price) profit
+from {{ ref('raw_order') }} as o 
+ join 
+    {{ ref('raw_customer') }} as c
+on o.customer_id = c.customer_id
+ join 
+    {{ ref('RAW_PRODUCT') }} as  p
+on p.product_id =o.product_id
