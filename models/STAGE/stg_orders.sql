b93d22307470078ea5c09f31e277378047f20610
Vijay S <vijaysha8791@gmail.com>
2024-05-12 16:53:15 +0000

    k

--- models/STAGE/stg_orders.sql
+++ models/STAGE/stg_orders.sql
@@ -1,10 +1,23 @@
-select o.*
-    , p.*
-    , (o.ORDER_COST_PRICE - o.order_selling_price) profit
+select 	o.ORDER_ID ,
+	o.ORDER_DATE,
+	o.SHIP_DATE,
+	o.SHIP_MODE ,
+	o.CUSTOMER_ID ,
+	o.ORDER_COST_PRICE ,
+	o.ORDER_SELLING_PRICE,
+	c.CUSTOMER_NAME ,
+	c.SEGMENT ,
+	c.COUNTRY ,
+	c.STATE ,
+   	p.CATEGORY ,
+	p.PRODUCT_ID ,
+	p.PRODUCT_NAME ,
+	p.SUBCATEGORY ,
+     (o.ORDER_COST_PRICE - o.order_selling_price) profit
 from {{ ref('raw_order') }} as o 
- join 
+ left join 
     {{ ref('raw_customer') }} as c
 on o.customer_id = c.customer_id
- join 
-    {{ ref('RAW_PRODUCT') }} as  p
-on p.product_id =o.product_id
+left join
+    {{ ref('RAW_PRODUCT') }} p
+on o.PRODUCT_ID = p.PRODUCT_ID
